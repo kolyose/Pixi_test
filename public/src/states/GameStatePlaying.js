d4c86c3e108ea9d487ee657e68e7225f3817eb9e
kols <kols@irace.com>
2017-03-31 11:43:01 +0300

    bug fix: reset fragments related functionality moved to entry of playing state

--- public/src/states/GameStatePlaying.js
+++ public/src/states/GameStatePlaying.js
@@ -5,6 +5,7 @@ export default class GameStatePlaying extends BaseGameState {
     super.entry();
     this._game.resetView();
     this._game.initView();
+    this._game.resetFragments();
     this._game.setupFragments();
     this._game.updateRoundCountdownView();
     this._game.startRoundCountdown();
